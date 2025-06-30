a5a8ec381f58af34ecc9bc126abeeb03871f9e30
rivkitoledano <rivkitoled@gmail.com>
2025-06-30 23:59:11 +0300

    longest increasing subquence

---
+++ AlgorithmPractice/LongestIncreasingSubqunce.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPractice
+{
+    internal class LongestIncreasingSubqunce
+    {
+        public int LongIncreasingSub(int[] nums, int idx, int prev)
+        {
+            if (idx < 0)
+                return 0;
+            if (nums[idx] < prev)
+                return Math.Max(LongIncreasingSub(nums, idx - 1, prev), 1 + LongIncreasingSub(nums, idx - 1, nums[idx]));
+            else
+                return LongIncreasingSub(nums, idx - 1, prev);
+        }
+
+        public int LongIncreasingSubRec(int[] nums)
+        {
+            int[] arr = new int[nums.Length];
+            arr[0] = 1;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                arr[i] = 1;
+                for (int j = 0; j < i; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        arr[i] = Math.Max(arr[i], arr[j] + 1);
+                    }
+                }
+            }
+            return arr.Max();
+            //if (idx < 0)
+            //    return 0;
+            //if (nums[idx] < prev)
+            //    return Math.Max(LongIncreasingSub(nums, idx - 1, prev), 1 + LongIncreasingSub(nums, idx - 1, nums[idx]));
+            //else
+            //    return LongIncreasingSub(nums, idx - 1, prev);
+        }
+    }
+}
---
+++ AlgorithmPractice/Program.cs
@@ -0,0 +1,9 @@
+﻿//כתוב פונקציה ב־C# שמקבלת מערך של מספרים שלמים ומחזירה את האורך של תת־הרצף העולה הכי ארוך (לא חייב להיות רצוף).
+using AlgorithmPractice;
+int[] nums = { 2, 5, 20, 3, 30, 4, -7, 8 };
+LongestIncreasingSubqunce longestIncreasingSubqunce = new LongestIncreasingSubqunce();
+
+int result = longestIncreasingSubqunce.LongIncreasingSub(nums, nums.Length-1, int.MaxValue);
+Console.WriteLine($"The length of the longest increasing subsequence is: {result}");
+int recResult = longestIncreasingSubqunce.LongIncreasingSubRec(nums);
+Console.WriteLine($"The length of the longest increasing subsequence using recursion is: {recResult}");
