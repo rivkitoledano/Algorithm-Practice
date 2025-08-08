7928cf8b3df1bb6dffd5fbf1d70237cb2254901d
rivkitoledano <rivkitoled@gmail.com>
2025-08-08 03:36:59 +0300

    valid parentheses string

---
+++ AlgorithmPractice/ValidParentheses.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPractice
+{
+    internal class ValidParentheses
+    {
+
+        static public bool IsValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+            foreach (char c in s)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    stack.Push(c);
+                }
+                else
+                {
+                    if (stack.Count == 0) return false;
+                    char top = stack.Pop();
+                    if ((c == ')' && top != '(') ||
+                        (c == '}' && top != '{') ||
+                        (c == ']' && top != '['))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
--- AlgorithmPractice/obj/Debug/net8.0/AlgorithmPractice.AssemblyInfo.cs
+++ AlgorithmPractice/obj/Debug/net8.0/AlgorithmPractice.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlgorithmPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43ec3de04cf30c4882de00ce61b818760e7dd2f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad6759c9b71fb2e56f22fe4f5845cda9f6018dc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlgorithmPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlgorithmPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
