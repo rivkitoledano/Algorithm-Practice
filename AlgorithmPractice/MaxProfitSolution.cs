4ac1c245addaa76fb7ce6fabd569e1ce7bdee752
rivkitoledano <rivkitoled@gmail.com>
2025-08-11 22:19:27 +0300

    Max Profit Solution

---
+++ AlgorithmPractice/MaxProfitSolution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPractice
+{
+    public class MaxProfitSolution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int minPrice = int.MaxValue;
+            int maxProfit = 0;
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (prices[i] < minPrice)
+                {
+                    minPrice = prices[i];
+                }
+                else
+                {
+                    maxProfit = Math.Max(prices[i] - minPrice, maxProfit);
+                }
+            }
+
+            return maxProfit;
+        }
+    }
+}
