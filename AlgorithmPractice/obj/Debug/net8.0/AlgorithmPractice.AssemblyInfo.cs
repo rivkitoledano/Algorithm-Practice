94fae267a5935964d05c51b416651fe406a8571a
rivkitoledano <rivkitoled@gmail.com>
2025-08-11 23:54:42 +0300

    3 array members - zero sum

--- AlgorithmPractice/obj/Debug/net8.0/AlgorithmPractice.AssemblyInfo.cs
+++ AlgorithmPractice/obj/Debug/net8.0/AlgorithmPractice.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlgorithmPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10b466b2f4ee90bdf205f213813db806f755e4a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ac1c245addaa76fb7ce6fabd569e1ce7bdee752")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlgorithmPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlgorithmPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
