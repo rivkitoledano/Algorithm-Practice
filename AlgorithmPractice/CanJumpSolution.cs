ce2d7ba7886825ba6ed78da41435003938475fa4
rivkitoledano <rivkitoled@gmail.com>
2025-08-12 00:55:49 +0300

    can jump solution

---
+++ AlgorithmPractice/CanJumpSolution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPractice
+{
+    internal class CanJumpSolution
+    {
+        public bool CanJump(int[] nums)
+        {
+            int maxPossible = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i > maxPossible)
+                {
+                    return false;
+                }
+                maxPossible = Math.Max(maxPossible, nums[i] + i);
+            }
+            return true;
+        }
+    }
+
+}
