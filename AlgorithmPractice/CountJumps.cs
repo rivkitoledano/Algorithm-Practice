554777d8541ba853c23e34d79fcd8a69fe568f07
rivkitoledano <rivkitoled@gmail.com>
2025-08-12 03:13:55 +0300

    count jumps in array - greedy solution

---
+++ AlgorithmPractice/CountJumps.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPractice
+{
+    internal class CountJumps
+    {
+        public int Jump(int[] nums)
+        {
+            int minSteps = 0;
+            int lengthArray = nums.Length - 1;
+            int maxArrived = 0;
+            int currentEnd = 0;
+            for (int i = 0; i < lengthArray; i++)
+            {
+                maxArrived = Math.Max(maxArrived, i + nums[i]);
+                if (currentEnd == i)
+                {
+                    minSteps++;
+                    currentEnd = maxArrived;
+                }
+
+            }
+            return minSteps;
+        }
+    }
+}
--- AlgorithmPractice/obj/Debug/net8.0/AlgorithmPractice.AssemblyInfo.cs
+++ AlgorithmPractice/obj/Debug/net8.0/AlgorithmPractice.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlgorithmPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce2d7ba7886825ba6ed78da41435003938475fa4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b7644d28267358fd2a4a66bac84391bdd4b466c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlgorithmPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlgorithmPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
