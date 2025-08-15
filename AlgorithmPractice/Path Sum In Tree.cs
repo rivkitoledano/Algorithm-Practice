9b44cbeddb5410b8d8a28acb1b70f694edf25e33
rivkitoledano <rivkitoled@gmail.com>
2025-08-15 15:40:49 +0300

    Path Sum in Tree Solution

---
+++ AlgorithmPractice/Path Sum In Tree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPractice
+{
+    internal class Path_Sum_In_Tree
+    {
+        public bool HasPathSum(TreeNode root, int targetSum)
+        {
+            if (root == null)
+                return false;
+
+            if (root.left == null && root.right == null)
+                return targetSum == root.val;
+
+            return HasPathSum(root.left, targetSum - root.val) ||
+                   HasPathSum(root.right, targetSum - root.val);
+        }
+
+    }
+}
--- AlgorithmPractice/obj/Debug/net8.0/AlgorithmPractice.AssemblyInfo.cs
+++ AlgorithmPractice/obj/Debug/net8.0/AlgorithmPractice.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlgorithmPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2947176b23ed3b678751b0e07beb7332f1e35903")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c0950455ecda3070fb19979f6dd4809bb69f7e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlgorithmPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlgorithmPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
