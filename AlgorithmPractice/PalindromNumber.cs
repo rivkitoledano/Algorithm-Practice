1b81c4bc3a280e7fea79a94cd954d8418dbbfe88
rivkitoledano <rivkitoled@gmail.com>
2025-08-18 02:29:25 +0300

    Palindrom Number Solution

---
+++ AlgorithmPractice/PalindromNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPractice
+{
+    internal class PalindromNumber
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0) return false;
+            string stringNum = x.ToString();
+            int start = 0, end = stringNum.Length - 1;
+            while (start < end)
+            {
+                if (stringNum[start] != stringNum[end])
+                    return false;
+                start++;
+                end--;
+            }
+            return true;
+        }
+    }
+}
