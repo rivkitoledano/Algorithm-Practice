46d696dfb185e464d0783b14117e10412b5f7ebe
rivkitoledano <rivkitoled@gmail.com>
2025-08-29 02:50:24 +0300

    Max Depth Solution

---
+++ AlgorithmPractice/MaxDepthSolution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPractice
+{
+    internal class MaxDepthSolution
+    {
+
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+            return Math.Max(MaxDepth(root.left), MaxDepth(root.right)) + 1;
+        }
+    }
+}
--- AlgorithmPractice/obj/Debug/net8.0/AlgorithmPractice.AssemblyInfo.cs
+++ AlgorithmPractice/obj/Debug/net8.0/AlgorithmPractice.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlgorithmPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58eda543c309d0072873f27e89170569bbb6a633")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+050c87720f86ed5583c6b5f615848d801840d161")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlgorithmPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlgorithmPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
